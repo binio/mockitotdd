ea7948c34b92f21239694879a810c8eed4915c4b
binio <tbiniecki@gmail.com>
2017-09-05 15:26:55 +0100

    refactored into one helper

--- testing/src/test/java/com/binio/testing/StringHelperTest.java
+++ testing/src/test/java/com/binio/testing/StringHelperTest.java
@@ -8,26 +8,21 @@ import static org.junit.Assert.*;
  * Created by tomasz.biniecki on 05/09/2017.
  */
 public class StringHelperTest {
-    @Test
-    public void truncateAInFirst2Positions() throws Exception {
-        //fail("Not implemented");
-    }
+
+    StringHelper helper = new StringHelper();
 
     @Test
     public void truncateAInFirstTwoPositions_whenTwoAPresentAtTheBegining() throws Exception {
-        StringHelper helper = new StringHelper();
         assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
     }
 
     @Test
     public void truncateAInFirstTwoPositions_whenOneAPresentAtTheBegining() throws Exception {
-        StringHelper helper = new StringHelper();
         assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
     }
 
     @Test
     public void truncateAInFirstTwoPositions_whenNoAPresentAtTheBegining() throws Exception {
-        StringHelper helper = new StringHelper();
         assertEquals("CDAA", helper.truncateAInFirst2Positions("CDAA"));
     }
 
