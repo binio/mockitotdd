c9e5ce68af8bbd78aaddf1293430ede5cb7631fc
binio <tbiniecki@gmail.com>
2017-09-05 11:48:10 +0100

    first test

---
+++ testing/src/test/java/com/binio/testing/StringHelperTest.java
@@ -0,0 +1,21 @@
+package com.binio.testing;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by tomasz.biniecki on 05/09/2017.
+ */
+public class StringHelperTest {
+    @Test
+    public void truncateAInFirst2Positions() throws Exception {
+        fail("Not implemented");
+    }
+
+    @Test
+    public void areFirstAndLastTwoCharactersTheSame() throws Exception {
+        fail("Not implemented");
+    }
+
+}
