15d45b29e1ffbaa075a8e76c603f7f1f08fac4fd
binio <tbiniecki@gmail.com>
2017-09-06 11:46:36 +0100

    add Mockito dependencies

---
+++ testing/src/test/java/com/binio/testing/FirstMockitoTest.java
@@ -0,0 +1,16 @@
+package com.binio.testing;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by tomasz.biniecki on 06/09/2017.
+ */
+public class FirstMockitoTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
