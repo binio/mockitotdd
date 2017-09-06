98e5c9de57c6bda08ec3e3d82673264f80fc5ce9
binio <tbiniecki@gmail.com>
2017-09-06 12:32:49 +0100

    setting up simple app for Mockito

---
+++ testing/src/main/java/com/binio/testing/TodoBusinessImpl.java
@@ -0,0 +1,31 @@
+package com.binio.testing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tomasz.biniecki on 06/09/2017.
+ */
+//TodoBusinessImpl SUT
+//TodoService - dependency
+
+public class TodoBusinessImpl {
+    private TodoService todoService;
+
+    public TodoBusinessImpl(TodoService service) {
+        super();
+        todoService =  service;
+    }
+
+    public List<String> retrieveTodosRealatedToSpring(String user) {
+        List<String> filteredTodos = new ArrayList<String>();
+        List<String> todos = todoService.retrievieTodos(user);
+
+        for(String todo:todos){
+            if(todo.contains("Spring")){
+                filteredTodos.add(todo);
+            }
+        }
+        return filteredTodos;
+    }
+}
---
+++ testing/src/main/java/com/binio/testing/TodoService.java
@@ -0,0 +1,11 @@
+package com.binio.testing;
+
+import java.util.List;
+
+/**
+ * Created by tomasz.biniecki on 06/09/2017.
+ */
+public interface TodoService {
+
+    public List<String> retrievieTodos(String user);
+}
