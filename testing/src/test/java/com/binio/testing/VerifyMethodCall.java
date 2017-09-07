a3ad4fc08d5ceee45e8f32d604eaddac9c3e97f9
binio <tbiniecki@gmail.com>
2017-09-07 17:05:33 +0100

    update dependency, add then() from BDDMockito

--- testing/src/test/java/com/binio/testing/VerifyMethodCall.java
+++ testing/src/test/java/com/binio/testing/VerifyMethodCall.java
@@ -10,6 +10,7 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
+import static org.mockito.BDDMockito.*;
 
 /**
  * Created by tomasz.biniecki on 07/09/2017.
@@ -31,5 +32,21 @@ public class VerifyMethodCall {
         verify(todoServiceMock).deleteTodo("Learn to dance");
         verify(todoServiceMock, never()).deleteTodo("Spring MVC");
         //verify(todoServiceMock).deleteTodo("Spring MVC"); this will not pass as delete removes only Spring elements
+    }
+
+    @Test
+    public void letsDeleteTodos_usingBDD() {
+        //Given
+        TodoService  todoServiceMock = mock(TodoService.class);
+        List<String> todos = Arrays.asList("Learn Spring", "Spring MVC", "Learn to dance");
+        given(todoServiceMock.retrievieTodos("Dummy")).willReturn(todos);
+        TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
+
+        //When
+        todoBusinessImpl.deleteTodosNotRealatedToSpring("Dummy");
+
+        //Then
+        then(todoServiceMock).should().deleteTodo("Learn to dance");
+        then(todoServiceMock).should(never()).deleteTodo("Spring MVC");
     }
 }
