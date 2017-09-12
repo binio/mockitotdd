591ba000577c8d3c01f8c417b32e689f695ca9b6
binio <tbiniecki@gmail.com>
2017-09-12 16:55:03 +0100

    add interface dependency to be mocked

---
+++ testing/src/main/java/com/binio/counter/NumGenerator.java
@@ -0,0 +1,9 @@
+package com.binio.counter;
+
+/**
+ * Created by tomasz.biniecki on 12/09/2017.
+ */
+public interface NumGenerator {
+    public int generate();
+    public int generateFriendly();
+}
