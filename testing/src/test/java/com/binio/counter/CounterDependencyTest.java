1981c3f33c93ebf3cd62466b20a6d91e26f7a728
binio <tbiniecki@gmail.com>
2017-09-12 16:56:19 +0100

    create Test and Mock interface and inject it into tested class

---
+++ testing/src/test/java/com/binio/counter/CounterDependencyTest.java
@@ -0,0 +1,23 @@
+package com.binio.counter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by tomasz.biniecki on 12/09/2017.
+ */
+public class CounterDependencyTest {
+
+    NumGenerator generator = mock(NumGenerator.class);
+
+    @Test
+    public void test() {
+        when(generator.generate()).thenReturn(6).thenReturn(5).thenReturn(0);
+        Counter counter = new Counter(generator);
+        assertEquals("OK", counter.getResult(counter.generator.generate()));
+        assertEquals("NOT OK", counter.getResult(counter.generator.generate()));
+    }
+}
