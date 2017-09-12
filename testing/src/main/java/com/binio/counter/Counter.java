de66cbb7d9012b3bcdf864e9d8425a030920daed
binio <tbiniecki@gmail.com>
2017-09-12 16:55:38 +0100

    add interface to Counter

--- testing/src/main/java/com/binio/counter/Counter.java
+++ testing/src/main/java/com/binio/counter/Counter.java
@@ -4,6 +4,16 @@ package com.binio.counter;
  * Created by tomasz.biniecki on 12/09/2017.
  */
 public class Counter {
+
+    public NumGenerator generator;
+
+    public Counter() {}
+
+    public Counter(NumGenerator generator) {
+        super();
+        this.generator = generator;
+    }
+
     public String getResult(int i) {
         if((i % 2)==0 && (i % 3)==0) {
             return "OK";
